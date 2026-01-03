6112cd21270b0848966531218b99431e066a914e
BahaaJber1 <bahaajber23@gmail.com>
2026-01-03 17:42:41 +0300

    update package.json

--- src/config/settings.js
+++ src/config/settings.js
@@ -17,6 +17,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD ?? "postgres";
 const DB_HOST = process.env.DB_HOST ?? "localhost";
 const DB_PORT = process.env.DB_PORT ?? 5432;
 const DB_NAME = process.env.DB_NAME ?? "kayan-health";
+const DB_CERT = process.env.DB_CERT ?? "";
 
 // Server Settings
 const PORT = process.env.PORT ?? 5000;
@@ -32,6 +33,7 @@ const databaseSettings = {
   DB_HOST,
   DB_PORT,
   DB_NAME,
+  DB_CERT,
 };
 
 const serverSettings = {
--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,69 @@
-import { databaseSettings } from "#config/settings";
+import { databaseSettings, environmentMode } from "#config/settings";
 import pg from "pg";
 
-const database = new pg.Pool({
+const ENVIRONMENT_MODE = environmentMode.MODE;
+
+const databaseDev = new pg.Pool({
   user: databaseSettings.DB_USER,
   host: databaseSettings.DB_HOST,
   database: databaseSettings.DB_NAME,
   password: databaseSettings.DB_PASSWORD,
   port: databaseSettings.DB_PORT,
+  // the lines below should be commented when not testing development AND online DB
+  ssl: {
+    rejectUnauthorized: true,
+    ca: databaseSettings.DB_CERT,
+  },
 });
+
+const databaseProd = new pg.Pool({
+  user: databaseSettings.DB_USER,
+  host: databaseSettings.DB_HOST,
+  database: databaseSettings.DB_NAME,
+  password: databaseSettings.DB_PASSWORD,
+  port: databaseSettings.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: databaseSettings.DB_CERT,
+  },
+});
+
+const database =
+  ENVIRONMENT_MODE === "development" ? databaseDev : databaseProd;
+
+await database.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(7) CHECK (role IN ('patient', 'doctor', 'finance')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS visits (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    patient_id uuid REFERENCES users(id) NOT NULL,
+    doctor_id uuid REFERENCES users(id) NOT NULL, -- comes from the frontend when select the doctor's name
+    date TIMESTAMP,
+    time VARCHAR(10),
+    patient_notes VARCHAR(500),
+    medical_notes VARCHAR(500),
+    status VARCHAR(15) CHECK (status IN ('completed', 'cancelled', 'pending', 'active')) DEFAULT 'pending',
+    amount SMALLINT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS treatments (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    visit_id uuid REFERENCES visits(id) ON DELETE CASCADE,
+    name VARCHAR(20) NOT NULL,
+    value SMALLINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`);
 
 export default database;
