72dd046cabdebf4d300f01c6f68d98ef7563cd83
BahaaJber1 <bahaajber23@gmail.com>
2026-01-04 15:38:01 +0300

    updated the store to remove the warning from express-session for production

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import { environmentMode, JWTSettings, serverSettings } from "#config/settings";
+import database from "#database/database";
 import passportInstance from "#middlewares/passport/localStrategy";
 import authenticationRouter from "#routes/authentication.routes";
 import baseRouter from "#routes/base.routes";
 import usersRouter from "#routes/users.route";
 import visitsRouter from "#routes/visits.routes";
+import connectPgSimple from "connect-pg-simple";
 import cors from "cors";
 import express from "express";
 import session from "express-session";
 
 const app = express();
 const PORT = serverSettings.PORT;
+
+const PgSession = connectPgSimple(session);
 
 app.use(
   cors({
@@ -22,6 +26,11 @@ app.use(express.json({ limit: "10kb" })); // adds size limit to the incoming JSO
 
 app.use(
   session({
+    store: new PgSession({
+      pool: database, // use existing database connection
+      tableName: "session", // table name
+      createTableIfMissing: true,
+    }),
     secret: JWTSettings.SESSION_SECRET, // Used to sign session ID cookie
     resave: false, // Don't save session if unmodified
     saveUninitialized: false,
